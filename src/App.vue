091d09ae52b3c1d3c9064c473992cb9c44d66b01
folkaras <efolkaras@gmail.com>
2019-12-09 23:27:47 +0300

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,36 @@
 <template>
-  <div class>
+  <div class="app">
+    <nav class="navbar">
+      <ul class="navbar__content">
+        <li class="navbar__item" v-for="(link, index) in links" :key="index">
+          <router-link class="navbar__link" :to="link.url">{{ link.title }}</router-link>
+        </li>
+      </ul>
+    </nav>
+		<section class="app__content">
+
     <router-view></router-view>
+		</section>
   </div>
 </template>
 
 <script>
 export default {
   name: "app",
-  components: {}
+  components: {},
+  data() {
+    return {
+      links: [
+        {
+          url: "/",
+          title: "home"
+        },
+        {
+          url: "/example",
+          title: "example"
+				},
+      ]
+    };
+  }
 };
 </script>
