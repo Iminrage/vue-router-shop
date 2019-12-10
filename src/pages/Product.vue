78980ef83dabb05945a13ad881c8fcaa52f7b7ff
folkaras <efolkaras@gmail.com>
2019-12-10 22:55:40 +0300

    chang template

--- src/pages/Product.vue
+++ src/pages/Product.vue
@@ -1,3 +1,23 @@
 <template>
-	<p>asdf</p>
+  <div class="product">
+    <div class="container">
+			<h2>{{ product.title }}</h2>
+			<p>{{ product.descr }}</p>
+			<img :src="product.img" :alt="product.title">
+		</div>
+  </div>
 </template>
+<script>
+export default {
+  name: "Product",
+  data() {
+    return {
+      product: null
+    };
+  },
+  created() {
+		let id = this.$route.params.id
+		this.product = this.$store.getters.getProduct(id)	
+  }
+};
+</script>
