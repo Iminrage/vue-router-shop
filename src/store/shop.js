85319c8642bdd6c5bba98fb1b67d75e676eaccf8
folkaras <efolkaras@gmail.com>
2019-12-10 22:56:05 +0300

    add getter

--- src/store/shop.js
+++ src/store/shop.js
@@ -12,7 +12,31 @@ export default{
 				title: "M2K",
 				descr: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore, provident?',
 				img: require('@/assets/img/m2k.webp')
-			}
+			},
+			{
+				id: 3,
+				title: "M2K",
+				descr: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore, provident?',
+				img: require('@/assets/img/m2k.webp')
+			},
+			{
+				id: 4,
+				title: "M2K",
+				descr: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore, provident?',
+				img: require('@/assets/img/m2k.webp')
+			},
+			{
+				id: 5,
+				title: "M2K",
+				descr: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore, provident?',
+				img: require('@/assets/img/m2k.webp')
+			},
+			{
+				id: 6,
+				title: "M2K",
+				descr: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore, provident?',
+				img: require('@/assets/img/m2k.webp')
+			},
 		]
 	},
 	mutations:{
@@ -24,6 +48,13 @@ export default{
 	getters: {
 		getShopList(state){
 			return state.shopList
+		},
+		getProduct: (state) => (id)=>{
+			return state.shopList.find(
+				el => el.id == id
+			);
 		}
+			
+		
 	}
 }
