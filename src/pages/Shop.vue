f7e02053bc2fcf232f0a2682917a8861e068969d
folkaras <efolkaras@gmail.com>
2019-12-10 22:55:54 +0300

    change template

--- src/pages/Shop.vue
+++ src/pages/Shop.vue
@@ -1,23 +1,27 @@
 <template>
-	<ul class="cards">
-		<ShopItem v-for="(item, index) in shopList" :key="index" :item="item"></ShopItem>
-	</ul>
+  <section>
+    <div class="container">
+      <ul class="cards">
+        <ShopItem v-for="(item, index) in shopList" :key="index" :item="item"></ShopItem>
+      </ul>
+    </div>
+  </section>
 </template>
 
 <script>
-import ShopItem from '@/components/ShopItem.vue'
+import ShopItem from "@/components/ShopItem.vue";
 export default {
-	name: "Shop",
-	components: {
-		ShopItem
-	},
-	data(){
-		return{
-			shopList: null
-		}
-	},
-	created(){
-		this.shopList = this.$store.getters.getShopList;
-	}
-}
+  name: "Shop",
+  components: {
+    ShopItem
+  },
+  data() {
+    return {
+      shopList: null
+    };
+  },
+  created() {
+    this.shopList = this.$store.getters.getShopList;
+  }
+};
 </script>
