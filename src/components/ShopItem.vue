183c9b48246fb790fc4953fc298e09189f922624
folkaras <efolkaras@gmail.com>
2019-12-10 14:14:36 +0300

    add card template

---
+++ src/components/ShopItem.vue
@@ -0,0 +1,24 @@
+<template>
+	<li class="cards__item">
+		<router-link class="cards__link" :to="`shop/${item.id}`">
+			<h3>{{ item.title }}</h3>
+			<p>{{ item.descr }}</p>
+			<img class="cards__img" :src="item.img" :alt="item.title">
+		</router-link>
+	</li>
+</template>
+<script>
+export default {
+	name: "ShopItem",
+	props: {
+		item: {
+			type: Object
+		}
+	},
+	data(){
+		return{
+
+		}
+	},
+}
+</script>
