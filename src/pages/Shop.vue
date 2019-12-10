f4474584129bf510ac6eaec6c7a624c7ef3165ef
folkaras <efolkaras@gmail.com>
2019-12-10 14:14:47 +0300

    add shop template

---
+++ src/pages/Shop.vue
@@ -0,0 +1,23 @@
+<template>
+	<ul class="cards">
+		<ShopItem v-for="(item, index) in shopList" :key="index" :item="item"></ShopItem>
+	</ul>
+</template>
+
+<script>
+import ShopItem from '@/components/ShopItem.vue'
+export default {
+	name: "Shop",
+	components: {
+		ShopItem
+	},
+	data(){
+		return{
+			shopList: null
+		}
+	},
+	created(){
+		this.shopList = this.$store.getters.getShopList;
+	}
+}
+</script>
